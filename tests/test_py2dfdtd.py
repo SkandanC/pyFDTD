5a6ecc0b9e058d9577a8bce048857fec36060bab
SkandanC <s39chand@edu.uwaterloo.ca>
2022-08-30 18:19:34 -0400

    Tests

---
+++ tests/test_py2dfdtd.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `pyfdtd` package."""
+
+
+import unittest
+
+from pyfdtd import solver
+
+
+class Testpyfdtd(unittest.TestCase):
+    """Tests for `pyfdtd` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
